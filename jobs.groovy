a844982412ec9229d51e9c889e0ae279e88ce017
Alexander Lubovsky <alexander.lubovsky@currency.com>
2019-06-28 14:54:21 +0300

    jenkins: testing node

--- jobs.groovy
+++ jobs.groovy
@@ -1,15 +1,16 @@
-pipeline {
-    agent any
+node {
+// pipeline {
+//    agent any
 
     parameters {
         string(defaultValue: "TEST", description: 'What environment?', name: 'userFlag')
         choice(choices: ['US-EAST-1', 'US-WEST-2'], description: 'What AWS region?', name: 'region')
     }
-    stages {
-        stage("foo") {
+//    stages {
+        stage("executiong echo command") {
             steps {
                 sh "echo ${params.region}"
             }
         }
-    }
+//    }
 }
