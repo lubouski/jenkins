26fb3478a2d470d36ed76a5f562fed6513ed1f56
Alexander Lubovsky <alexander.lubovsky@currency.com>
2019-06-28 12:09:37 +0300

    jenkins: test groovy job added

---
+++ jobs.groovy
@@ -0,0 +1,15 @@
+pipeline {
+    agent any
+
+    parameters {
+        string(defaultValue: "TEST", description: 'What environment?', name: 'userFlag')
+        choice(choices: ['US-EAST-1', 'US-WEST-2'], description: 'What AWS region?', name: 'region')
+    }
+    stages {
+        stage("foo") {
+            steps {
+                sh "echo ${params.region}"
+            }
+        }
+    }
+}
