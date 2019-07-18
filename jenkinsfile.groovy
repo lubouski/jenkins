c96bc383e1a183527b080274aa940f2f6fa904c4
Alexander Lubovsky <alexander.lubovsky@currency.com>
2019-07-18 17:32:19 +0300

    jenkins: function refactoringv0.14

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,7 +1,6 @@
 node {
          stage("test") {
-             checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/lubouski/jenkins.git']]])
-}   
+             checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/lubouski/jenkins.git']]])   
              sh "ls"
          }
 
