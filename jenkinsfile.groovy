c5a301c5f2dd180d1a93526328722c2ecd20efcf
Alexander Lubovsky <alexander.lubovsky@currency.com>
2019-07-18 16:32:19 +0300

    jenkins: function refactoringv0.1

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,9 +1,9 @@
 pipeline {
     agent any
-    def function_call = load 'Jenkinsfile.common.groovy' 
+    def pipeline = load 'Jenkinsfile.common.groovy' 
     stages {
         stage("execution function") {
-        function_call.exec_bash(hello, chubakka-333)
+        pipeline.exec_bash(hello, chubakka-333)
         }
     }
 }
