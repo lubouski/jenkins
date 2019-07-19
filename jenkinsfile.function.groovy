69372effb9f1d4393acaab5676a4251dfbbf91bc
Alexander Lubovsky <alexander.lubovsky@currency.com>
2019-07-19 10:45:45 +0300

    jenkins: env.variable passing to functionv0.2

--- jenkinsfile.function.groovy
+++ jenkinsfile.function.groovy
@@ -1,7 +1,9 @@
 echo 'common loaded'
 
 def exec_bash(input_word, id) {
-    sh "echo $input_word $id"
+    withCredentials([file(credentialsId: "fsn1-1-kube-currency-exchange-test.config", variable: 'KUBE_CONFIG')]) {
+    sh "echo $input_word $id $KUBE_CONFIG"
+    }
 }
 // important to not forget "return this"
 return this
