8566d84616bc17a345908948ccfc98583084d908
Alexander Lubovsky <alexander.lubovsky@currency.com>
2019-07-19 13:25:31 +0300

    jenkins: added promotion step in jenkins UI

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -8,6 +8,22 @@ node {
          def pipeline = load 'jenkinsfile.function.groovy'
          def isBootstrap = "true"
    
+        stage('Deploy promotion: test execution') {
+            timeout(time:1, unit:'DAYS') {
+                test_approve = input(
+                    message: "Deploy to TEST?",
+                    submitterParameter: 'approved_by',
+                    parameters: [
+                        [$class: 'TextParameterDefinition', defaultValue: "NO", description: 'Please type YES to continue with TEST', name: 'exchange']
+                    ]
+                )
+            }
+            if (test_approve.exchange != "YES") {
+                currentBuild.result = 'ABORTED'
+                error ("Build was aborted")
+            }
+        }
+
          stage("execution function") {
              pipeline.exec_bash('hello', "$BUILD_ID", "$isBootstrap", "fsn1-1-kube-currency-exchange-test.config")
          }
